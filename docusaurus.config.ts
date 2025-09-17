b3fe846a85ab291afc3ac44074fd80e9cdd7f580
Simon <104859696+py-bay@users.noreply.github.com>
2025-09-17 20:33:16 +0200

    45 document usage of joy standard buttons in shared components documentation (#46)
    closes #45 
    
    also fix link to repo so edit this page works now

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,10 +63,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Agile-Software-Engineering-25/documentation/edit/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
